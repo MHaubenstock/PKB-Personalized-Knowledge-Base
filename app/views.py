e14151b8ad0ca968d5cd6a376a1884edd1f52b19
Thomas D <tdietert@mail.usf.edu>
2014-11-25 23:08:21 -0500

    Fixed Bootstrap not displaying correctly
    To Do:
    Split up Create_new_topic and Edit_topic
    Fix CSS on Create_new_topic, Edit_topic
    Decide on Final changes

--- app/views.py
+++ app/views.py
@@ -24,13 +24,13 @@ def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
 
-@app.route('/home', methods = ['GET', 'POST'])
+@app.route('/home/<username>', methods = ['GET', 'POST'])
 @login_required
-def home():
+def home(username):
     user = g.user
     #Get top level topic titles
     topLevelTopicTitles = [t for t in user.topics]
-    return render_template('home.html', topics=topLevelTopicTitles)
+    return render_template('home.html', username=user.username, topics=topLevelTopicTitles)
 
 
 # this is the default page that shows up when connecting
@@ -40,7 +40,7 @@ def home():
 def login():
     # if user is logged in, redirect them to home
     if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('home'))
+        return redirect(url_for('home', username=g.user.username))
     form = LoginForm()
     # tries to login user
     if form.validate_on_submit():
@@ -56,7 +56,7 @@ def login():
                     session.pop('remember_me', None)
                 login_user(registered_user, remember = remember_me)
                 flash('Logged in successfully')
-                return redirect(request.args.get('next') or url_for('home'))
+                return redirect(request.args.get('next') or url_for('home', username=registered_user.username))
             else:
                 form.password.errors.append("Invalid password!")
         else:
@@ -67,7 +67,7 @@ def login():
 def register():
     # if user is logged in, redirect them to home
     if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('home'))
+        return redirect(url_for('home', username=g.user.username))
     form = RegisterForm()
     # tries to register user
     if form.validate_on_submit():
@@ -87,74 +87,63 @@ def register():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
-@app.route('/<topicParent>/<topic>', methods = ['GET', 'POST'])
+@app.route('/<topicParent>/<topic_name>', methods = ['GET', 'POST'])
 @login_required
-def topic(topic, topicParent):
-    thisTopic = UserTopic.query.filter(UserTopic.title == topic).first()
+def topic(topic_name, topicParent):
+    topic = UserTopic.query.filter(UserTopic.title == topic_name).first()
     
-    if thisTopic:
-        description = thisTopic.description
-        tags = thisTopic.tags
-        topicParentParent = UserTopic.query.filter(UserTopic.title == topic).first().parent
-    # why would they be able to click on a topic that doesn't exist?
-    #If they try typing the topic into the address bar and it's incorrect it will redirect to the home page
-    else: #Did not find topic, don't go to page
-        return redirect(url_for('home'))
+    if topic is None:
+        return redirect(url_for('home', username=g.user.username))
 
-    subTopics = [s.title for s in UserTopic.query.filter(UserTopic.parent == topic).all()]
+    description = topic.description
+    tags = topic.tags
+    subTopics = [s.title for s in UserTopic.query.filter(UserTopic.parent == topic_name).all()]
+    topicParentParent = UserTopic.query.filter(UserTopic.title == topic_name).first().parent
 
     return render_template('topic.html', topic = topic, topicParent = topicParent, topicParentParent = topicParentParent, subTopics = subTopics, description = description, tags = tags)
 
-
-""" 
-We need to think about adding "bread crumbs" to the site, for now 
-we should leave this url as it is, but notice the bug in the url.
-because sometimes there is no we need a way of generating a long url
-with all the topic parents chained. Or maybe we don't need to, and we implement
-a topic list of links to the respective topic page.
-||||||||||||||||||||||||||||||||||||||||||||||||||
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-"""
 @app.route('/<user>/create_new_topic', methods = ['GET', 'POST'])
 @app.route('/<topicParent>/<topic>/edit_topic', methods = ['GET', 'POST'])
 @login_required
-def edittopic(user=None,topic=None,topicParent=None):
+def edittopic(user=None,topic_name=None,topicParent=None):
     #For submitting topic data
     form = EditTopic()
-    theTopic = UserTopic.query.filter(UserTopic.title == topic).first()
+    topic = UserTopic.query.filter(UserTopic.title == topic_name).first()
     
-    if theTopic:
+    if topic:
         if theTopic.tags:
-            tags = ', '.join(theTopic.tags)
+            tags = ', '.join(topic.tags)
         else:
             tags = ''
     
-        description = theTopic.description
+        description = topic.description
     else:
         topic = ''
         tags = ''
         description = ''
-        #not passing topicparent to the function when it's not part of the route for some reason, so need to get it manually
+
+        # not passing topicparent to the function when it's not part of the route for 
+        # some reason, so need to get it manually
         topicParent = request.args.get('topicParent')
 
     #couldn't get if form.validate_on_submit(): working
     if request.method == 'POST': #and form.validate():
         #if it's a new topic
-        if not theTopic:
-            theTopic = UserTopic(form.topicTitle.data, topicParent, [])
-            db.session.add(theTopic)
+        if topic is None:
+            topic = UserTopic(form.topicTitle.data, topicParent, [])
+            db.session.add(topic)
 
         #Save topic information to the topic
-        theTopic.title = form.topicTitle.data
-        theTopic.tags = list(filter(lambda x: not x == '', form.tags.data.replace(' ', '').split(',')))
-        theTopic.description = form.description.data
-        theTopic.parent = topicParent
+        topic.title = form.topicTitle.data
+        topic.tags = list(filter(lambda x: not x == '', form.tags.data.replace(' ', '').split(',')))
+        topic.description = form.description.data
+        topic.parent = topicParent
 
         #if it's a top level topic, add it to user topics
-        if theTopic.parent == g.user.username:
-            g.user.topics.append(theTopic)
+        if topic.parent == g.user.username:
+            g.user.topics.append(topic)
         
         db.session.commit()
 
